9c94c72f5b8c53fafdbbfa85c2733f01b0cfa09b
NizarDawood <77719056+NizarDawood@users.noreply.github.com>
2022-08-26 13:19:03 +0200

    08/26
    jag har funderat på hur man löser problemet gjort en scanner. sen fundera jag över hur jag coda för att svara på input med ett if statement det blev svår så jag behöver repetera grunderna med hur man skriver java såsom ifsatser metoder

---
+++ src/peppet.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class peppet {
+    public static void main(String[] args) {
+        System.out.println("väkommen till din digitala psykolog vad kan jag hjälpa dig med ");
+        //förklara hans problem behöver använda scannaer för att få input
+        Scanner problem = new Scanner(System.in);
+        String in = problem.nextLine();
+        //statement
+
+       int nummer;
+       nummer = 1;
+
+// input ska besvaras med ok
+        if ( in = nummer ){
+            System.out.println(  "ok"  );}
+
+
+
+
+    }
+
+}
