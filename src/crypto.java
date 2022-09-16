a8168810d54184a804284d535017623a38ed5738
NizarDawood <77719056+NizarDawood@users.noreply.github.com>
2022-09-16 13:29:18 +0200

    9/16

--- src/crypto.java
+++ src/crypto.java
@@ -1,22 +1,32 @@
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+
+// The Model performs all the calculations needed
+// and that is it. It doesn't know the View
+// exists
 public class crypto {
+    // Holds the value of the sum of the numbers
+    // skriva ut två ord som är crypterade i binära tal.    binära talet minus
+    // key för att få ut det crypterade ordet
+    private static int calculationValue;
+
+    String message= "hej";
+    String key="da";
+
+    public void addTwoNumbers(int x, int y){
+        calculationValue = x + y;
+    }
+
+    public int getCalculationValue() {
+        return calculationValue;
+    }
     public static void main(String[] args) {
-
-
-        char x ='b';
-        char y = 'c';
-
-        System.out.println(Integer.toBinaryString(y));
-        System.out.println(Integer.toBinaryString(x));
-        System.out.println(Integer.toBinaryString(x&y));
-        System.out.println(Integer.toBinaryString(y/x));
-
-
-        // decryptera
-
-
-
-
-
-
+        System.out.println(Integer.toBinaryString(calculationValue));
     }
 }
+
+
+
+
+
+//filnam medelande key
+
