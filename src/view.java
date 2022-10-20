cba92b3eea92245cefbc51cad47756b525196a1c
NizarDawood <77719056+NizarDawood@users.noreply.github.com>
2022-10-20 13:41:00 +0200

    10/20

--- src/view.java
+++ src/view.java
@@ -1,30 +1,68 @@
 import javax.swing.*;
 
 public class view {
-    private JTextArea text = new JTextArea();
-    private JTextField key = new JTextField();
-    private JButton button = new JButton();
-    private JTextPane svar = new JTextPane();
-    private JPanel mainPanel = new JPanel();
 
-    controllerview(){
-        calcPanel.add(text);
-        calcPanel.add(key);
-        calcPanel.add(button);
-        calcPanel.add(svar);
+    public static void main(String[] args) {
+
 
     }
-        JPanel calcPanel = new JPanel();
 
-        public int getText() {
-            return Integer.parseInt(text.getText());
-        }
+    private JPanel mainPanel;
+    private JTextArea text;
+    private JTextField key;
+    private JButton button;
+    private JTextPane svar;
 
-        public int getKey() {
-            return Integer.parseInt(key.getText());
-        }
-    public void setCalcSolution(int solution) {
-        calcSolution.setText(Integer.toString(solution));
+
+    public view() {
+
     }
+        public JPanel mainPanel () {
+            return mainPanel;
+        }
 
+
+        public String getText () {
+            return text.getText();
+        }
+        public String getKey () {
+            return key.getText();
+        }
+        public void setsvar (String c){
+            svar.setText(c);
+        }
+
+
+    }
 }
+
+
+
+//public class view {
+//    private JTextArea text = new JTextArea();
+//    private JTextField key = new JTextField();
+//    private JButton button = new JButton();
+//    private JTextPane svar = new JTextPane();
+//    private JPanel mainPanel = new JPanel();
+//
+//    controllerview(){
+//        calcPanel.add(text);
+//        calcPanel.add(key);
+//        calcPanel.add(button);
+//        calcPanel.add(svar);
+//
+//    }
+//        JPanel calcPanel = new JPanel();
+//
+//        public int getText() {
+//            return Integer.parseInt(text.getText());
+//        }
+//
+//        public int getKey() {
+//            return Integer.parseInt(key.getText());
+//        }
+//    public void setCalcSolution(int solution) {
+//        calcSolution.setText(Integer.toString(solution));
+//    }
+//
+//}
